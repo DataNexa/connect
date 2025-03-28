6779b7397211e0e3664abbb4ec7a8c39ac4aa0a8
Andrei Coelho <andreifcoelho@gmail.com>
2025-03-28 19:57:46 -0300

    estrutura inicial finalizada

---
+++ src/Main.vue
@@ -0,0 +1,17 @@
+<template>
+    <mainpage></mainpage>
+</template>
+
+<script lang="ts">
+
+import mainpage from '@/pages/Index.vue'
+
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+
+    components: { mainpage }
+
+})
+
+</script>
